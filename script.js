1dcdf802dabd109d6828fa3df53d113271913e7d
iamjohnnysky <vanpasie20@gmail.com>
2025-03-04 11:35:34 -0800

    creating a responsive portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMeun() {
+    const meun = document.querySelector(".meun-links");
+    const icon = document.querySelector(".hamburger-icon");
+    meun.classList.toggle("open");
+    icon.classList.toggle("open");
+}
